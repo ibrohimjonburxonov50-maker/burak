7c92e5e26deb8c034456648e3a62f9692e57af4d
ibrohimjon <ibrohimjonburxonov50@gmail.com>
2026-09-25 20:12:08 +0900

    fix: modify routing system and cotrollers

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import path from "path";
+import routerAdmin from "./routerAdmin";
 
-/**1-ENTRANCE **/
+/* 1-ENTRANCE */
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/** 2-SESSIONS **/
-
-/** 3-VIEWS **/
+/* 3-VIEWS */
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-/** 4-ROUTERS **/
+/* 4-ROUTERS */
+app.use("/admin", routerAdmin); // EJS
+// app.use("/", router); // REACT
 
 export default app;
